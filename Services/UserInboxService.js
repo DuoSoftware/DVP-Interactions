a0f4d68d0128b8d6b0334d89f94378ec0156e7ab
dinusha_dck <dinusha@duosoftware.com>
2016-09-12 21:04:28 +0530

    add additional methods required for mail inbox app

--- Services/UserInboxService.js
+++ Services/UserInboxService.js
@@ -8,6 +8,7 @@ var InboxMessage = require('dvp-mongomodels/model/UserInbox').InboxMessage;
 var User = require('dvp-mongomodels/model/User');
 var messageFormatter = require('dvp-common/CommonMessageGenerator/ClientMessageJsonFormatter.js');
 var uuid = require('node-uuid');
+var async = require('async');
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
@@ -66,6 +67,13 @@ function AddMessageToInbox(req, res, next)
 
 
             }
+            else
+            {
+                var jsonString = messageFormatter.FormatMessage(new Error('User not found'), "User not found", false, false);
+                logger.debug('[DVP-Interactions.AddMessageToInbox] - [%s] - API RESPONSE : %s', reqId, jsonString);
+                res.end(jsonString);
+
+            }
 
         });
 
@@ -79,7 +87,7 @@ function AddMessageToInbox(req, res, next)
 
     return next();
 
-};
+}
 
 function SetMessageAsRead(req, res, next)
 {
@@ -90,8 +98,8 @@ function SetMessageAsRead(req, res, next)
 
         var companyId = req.user.company;
         var tenantId = req.user.tenant;
-        var profileId = req.body.profile;
-        var msgId = req.body.messageId;
+        var profileId = req.params.profile;
+        var msgId = req.params.messageId;
 
         if (!companyId || !tenantId)
         {
@@ -130,7 +138,7 @@ function SetMessageAsRead(req, res, next)
 
     return next();
 
-};
+}
 
 function DeleteMessage(req, res, next)
 {
@@ -181,7 +189,7 @@ function DeleteMessage(req, res, next)
 
     return next();
 
-};
+}
 
 //Bind Function
 var SendGetMessagesResponse = function(res, reqId, err, msgs)
@@ -198,10 +206,115 @@ var SendGetMessagesResponse = function(res, reqId, err, msgs)
         logger.debug('[DVP-Interactions.SendGetMessagesResponse] - [%s] - API RESPONSE : %s', reqId, 'MESSAGES FOUND - NOT PRINTING DUE TO PERFORMANCE');
         res.end(jsonString);
     }
+};
+
+var getUnreadCount = function(profileId, companyId, tenantId, callback)
+{
+    InboxMessage.count({company: companyId, tenant: tenantId, profile: profileId, has_read: 'false', message_state: 'RECEIVED'},
+        function (err, unreadCount)
+        {
+            callback(err, unreadCount);
+        });
+};
+
+var getAllCount = function(profileId, companyId, tenantId, callback)
+{
+    InboxMessage.count({company: companyId, tenant: tenantId, profile: profileId, message_state: 'RECEIVED'},
+        function (err, allCount)
+        {
+            callback(err, allCount);
+        });
+};
+
+var getDeleteCount = function(profileId, companyId, tenantId, callback)
+{
+    InboxMessage.count({company: companyId, tenant: tenantId, profile: profileId, message_state: 'DELETED'},
+        function (err, allCount)
+        {
+            callback(err, allCount);
+        });
+};
+
+var getCountByType = function(profileId, companyId, tenantId, msgType, callback)
+{
+    InboxMessage.count({company: companyId, tenant: tenantId, profile: profileId, message_state: 'RECEIVED', message_type: msgType},
+        function (err, allCount)
+        {
+            callback(err, allCount);
+        });
+};
+
+
+function GetMessageInboxCounts(req, res, next)
+{
+    var reqId = uuid.v1();
+    var emptyArr = [];
+    try
+    {
+        logger.debug('[DVP-Interactions.GetInboxCounts] - [%s] - HTTP Request Received', reqId);
+
+        var companyId = req.user.company;
+        var tenantId = req.user.tenant;
+        var profileId = req.params.profileId;
+        var arr = [];
+
+        if (!companyId || !tenantId)
+        {
+            throw new Error("Invalid company or tenant");
+        }
+
+        arr.push(getUnreadCount.bind(this, profileId, companyId, tenantId));
+        arr.push(getAllCount.bind(this, profileId, companyId, tenantId));
+        arr.push(getDeleteCount.bind(this, profileId, companyId, tenantId));
+        arr.push(getCountByType.bind(this, profileId, companyId, tenantId, 'FACEBOOK'));
+        arr.push(getCountByType.bind(this, profileId, companyId, tenantId, 'TWITTER'));
+
+        async.parallel(arr, function(err, results)
+        {
+            if(err)
+            {
+                var jsonString = messageFormatter.FormatMessage(err, "ERROR", false, null);
+                logger.error('[DVP-Interactions.GetUnReadMessages] - [%s] - API RESPONSE : %s', reqId, jsonString);
+                res.end(jsonString);
+            }
+            else
+            {
+                if(results && results.length === 5)
+                {
+                    var countsObj = {
+                        Unread: results[0],
+                        All: results[1],
+                        Deleted: results[2],
+                        Facebook: results[3],
+                        Twitter: results[4]
+                    };
+
+                    var jsonString = messageFormatter.FormatMessage(null, "SUCCESS", true, countsObj);
+                    logger.error('[DVP-Interactions.GetUnReadMessages] - [%s] - API RESPONSE : %s', reqId, jsonString);
+                    res.end(jsonString);
+                }
+                else
+                {
+                    var jsonString = messageFormatter.FormatMessage(null, "SUCCESS", true, null);
+                    logger.error('[DVP-Interactions.GetUnReadMessages] - [%s] - API RESPONSE : %s', reqId, jsonString);
+                    res.end(jsonString);
+                }
+            }
+
+        });
+
+
+    }
+    catch(ex)
+    {
+        var jsonString = messageFormatter.FormatMessage(ex, "ERROR", false, emptyArr);
+        logger.error('[DVP-Interactions.GetUnReadMessages] - [%s] - API RESPONSE : %s', reqId, jsonString);
+        res.end(jsonString);
+    }
+
+    return next();
+
 }
-
-
-
 
 function GetUnreadMessages(req, res, next)
 {
@@ -267,7 +380,7 @@ function GetUnreadMessages(req, res, next)
 
     return next();
 
-};
+}
 
 function GetReadMessages(req, res, next)
 {
@@ -333,7 +446,83 @@ function GetReadMessages(req, res, next)
 
     return next();
 
-};
+}
+
+function GetInboxMessages(req, res, next)
+{
+    var reqId = uuid.v1();
+    var emptyArr = [];
+    try
+    {
+        logger.debug('[DVP-Interactions.GetInboxMessages] - [%s] - HTTP Request Received', reqId);
+
+        var companyId = req.user.company;
+        var tenantId = req.user.tenant;
+        var profileId = req.params.profileId;
+        var limitCount = req.query.limit;
+        var sinceId = req.query.since;
+        var msgType = req.query.messageType;
+
+        if (!companyId || !tenantId)
+        {
+            throw new Error("Invalid company or tenant");
+        }
+
+        var cond = {company: companyId, tenant: tenantId, profile: profileId, message_state: 'RECEIVED'};
+
+        if(msgType)
+        {
+            cond.message_type = msgType;
+        }
+
+        if(sinceId)
+        {
+            cond._id = { $gt: sinceId };
+            if(limitCount)
+            {
+                InboxMessage.find(cond)
+                    .limit(limitCount)
+                    .populate('engagement_session')
+                    .exec(SendGetMessagesResponse.bind(this, res, reqId));
+            }
+            else
+            {
+                InboxMessage.find(cond)
+                    .populate('engagement_session')
+                    .exec(SendGetMessagesResponse.bind(this, res, reqId));
+            }
+        }
+        else
+        {
+            if(limitCount)
+            {
+                InboxMessage.find(cond)
+                    .limit(limitCount)
+                    .populate('engagement_session')
+                    .exec(SendGetMessagesResponse.bind(this, res, reqId));
+            }
+            else
+            {
+                InboxMessage.find(cond)
+                    .populate('engagement_session')
+                    .exec(SendGetMessagesResponse.bind(this, res, reqId));
+            }
+        }
+
+
+
+    }
+    catch(ex)
+    {
+        var jsonString = messageFormatter.FormatMessage(ex, "ERROR", false, emptyArr);
+        logger.error('[DVP-Interactions.GetInboxMessages] - [%s] - API RESPONSE : %s', reqId, jsonString);
+        res.end(jsonString);
+    }
+
+    return next();
+
+}
+
 
 function GetDeletedMessages(req, res, next)
 {
@@ -399,7 +588,7 @@ function GetDeletedMessages(req, res, next)
 
     return next();
 
-};
+}
 
 
 
@@ -409,3 +598,5 @@ module.exports.GetReadMessages = GetReadMessages;
 module.exports.GetDeletedMessages = GetDeletedMessages;
 module.exports.SetMessageAsRead = SetMessageAsRead;
 module.exports.DeleteMessage = DeleteMessage;
+module.exports.GetInboxMessages = GetInboxMessages;
+module.exports.GetMessageInboxCounts = GetMessageInboxCounts;
